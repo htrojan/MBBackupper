aade0f65519532848899cefefbb29f3f51b425ff
lobbi44 <hendyGAweb.de>
2014-11-01 12:07:46 +0100

    ReSharper Enabled

---
+++ Serializer/SerializerI/ISpecialAttributeHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serializer.TypeParser;
+
+namespace Serializer.SerializerI
+{
+    interface ISpecialAttributeHandler
+    {
+        void Execute(ref AssemblyPart part, SerializerAttribute attribute);
+        bool CanExecute(SerializerAttribute attribute);
+    }
+}
